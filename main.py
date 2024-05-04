6b5bd907b2974536e706e6967ed6e9160256a1df
Raksha Rehal <raksha.rehal@gmail.com>
2024-05-04 17:44:50 -0400

    added a display for ease of use :)

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from playwright.sync_api import sync_playwright
 import re
 import time
 import html
+import tkinter as tk
+from tkinter.scrolledtext import *
+import os, shutil
+
+root=tk.Tk()
+
+# setting the windows size
+root.geometry("635x375")
+root.title("Wattpad PMs Saver")
+
+# declaring string variable
+# for storing name and password
+name_var=tk.StringVar()
+passw_var=tk.StringVar()
+status_var=tk.StringVar()
+
 
 # https://www.youtube.com/watch?v=H2-5ecFwHHQ
 
@@ -31,6 +47,7 @@ def gather_single_logs(username, password, all_users):
 
                 flag = True
                 offset = 0
+
                 while flag:
                     content = page.content()
                     make_files(content, party2)
@@ -114,28 +131,101 @@ def clean_logs(content):
         pm['message'] = message
 
         cleaned.append(pm)
-    
-    reversed_clean = list(reversed(cleaned))
 
     return cleaned
 
 
-def main():
+def submit():
 
-    parties = open("usernames.txt", "r")
-    all_users = parties.read().splitlines()
-
-    auth = open("authentication.txt", "r")
-    userPass = auth.read().splitlines()
+    username=name_var.get()
+    password=passw_var.get()
+    try:
+        users=users_entry_scrolled.get('1.0', 'end')
     
-    # username, password, all users
-    status = gather_single_logs(userPass[0], userPass[1], all_users)
+    except:
+        pass
+    
+    status = main(username, password, users)
 
-    if status:
-        print('Logs saved!')
+    if not status:
+        status_var.set("There was a critical error with saving the logs.")
     else:
-        print('Error - could not save logs.')
-    
+        status_var.set("Logs saved! Find them in the /saved_logs folder.")
 
-main()
+
+def clear_logs():
+
+    if os.path.isdir("saved_logs"):
+        shutil.rmtree("saved_logs")
+        os.mkdir("saved_logs")
+    
+    status_var.set("All saved logs were deleted.")
+
+
+
+def main(username, password, pmusers):
+    
+    all_users=pmusers.split()
+    # username, password, all users
+    status = gather_single_logs(username, password, all_users)
+
+    return status
+
+
+# creating a label for 
+# name using widget Label
+name_label = tk.Label(root, text = 'Username', font=('calibre',10, 'bold'))
+
+# creating a entry for input
+# name using widget Entry
+name_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
+
+# creating a label for password
+passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'bold'))
+
+# creating a entry for password
+passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+
+# creating a label for 
+# name using widget Label
+users_label = tk.Label(root, text = 'Participating PM Users', font=('calibre',10, 'bold'))
+space = tk.Label(root, text = ' ', font=('calibre',10, 'bold'))
+users_desc1 = tk.Label(root, text = 'Please enter the usernames of those whom you wish to save your PMs with.', font=('calibre',10))
+users_desc2 = tk.Label(root, text = 'SEPARATE USERNAMES WITH A SPACE, for example: username1 username2 username3', font=('calibre',10))
+users_desc3 = tk.Label(root, text = 'For best results, input all usernames in one go. If you make a mistake, press the delete button and try again.', font=('calibre',10))
+
+
+users_entry_scrolled = ScrolledText(root, font = ('calibre',10,'normal'), width = 37, height = 5)
+
+
+# creating a button using the widget 
+# Button that will call the submit function 
+sub_btn=tk.Button(root,text = 'Submit', command = submit)
+
+clear=tk.Button(root,text = 'Delete Current Saved Logs', command = clear_logs)
+
+final_status = tk.Label(root, textvariable=status_var, font=('calibre',10, 'bold'))
+
+# placing the label and entry in
+# the required position using grid
+# method
+name_label.grid(row=0,column=0)
+name_entry.grid(row=1,column=0)
+passw_label.grid(row=2,column=0)
+passw_entry.grid(row=3,column=0)
+space.grid(row=4,column=0)
+users_label.grid(row=5,column=0)
+users_desc1.grid(row=6,column=0)
+users_desc2.grid(row=7,column=0)
+users_desc3.grid(row=8,column=0)
+users_entry_scrolled.grid(row=9,column=0)
+space.grid(row=10,column=0)
+sub_btn.grid(row=11,column=0)
+clear.grid(row=12,column=0)
+space.grid(row=13,column=0)
+final_status.grid(row=14,column=0)
+# performing an infinite loop 
+# for the window to display
+
+root.mainloop()
 
